5dfdfecc11f55abe4a27579bea4ffdc7e39f2a54
Akos Faddi <faddiakos@gmail.com>
2021-03-19 16:26:09 +0100

    bmiCalc more resp+updEng

--- myFirstApp/bmicalc/script.js
+++ myFirstApp/bmicalc/script.js
@@ -15,11 +15,11 @@ function bmi() {
 
 function evaluation(bmi) {
     if (bmi<18.5) {
-        document.getElementById("res").innerHTML += `<br><br>You're lean. Go and eat something!`;
+        document.getElementById("res").innerHTML += `<br><br>You're underweight. Go and eat something!`;
     } else if (bmi<25) {
-        document.getElementById("res").innerHTML += `<br><br>You're normal. Keep up the good work!`;
+        document.getElementById("res").innerHTML += `<br><br>Congrats! Your weight is normal. Keep up the good work!`;
     } else if (bmi<30) {
-        document.getElementById("res").innerHTML += `<br><br>You're overweighted. Go to the gym!`;
+        document.getElementById("res").innerHTML += `<br><br>You're overweight. Go to the gym!`;
     } else {
         document.getElementById("res").innerHTML += `<br><br>You're obese. Stop drinking that much beer!`;
     }
