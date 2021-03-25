73934908db298840e8b4c1606a1cac3308fa2711
Akos Faddi <faddiakos@gmail.com>
2021-03-25 04:27:04 +0100

    gallery v4
    some responsibility
    bugfixes
    also some new bugs

--- myFirstApp/gallery/script.js
+++ myFirstApp/gallery/script.js
@@ -4,12 +4,12 @@ let galleryArr = [
     {
         image: "./img/DSC_0412.JPG",
         title: "Piano",
-        paragraph: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem in eveniet sed nobis. Itaque, autem! Sapiente, non repellendus! Quas totam doloremque adipisci necessitatibus eius ad, quasi nobis illum quam itaque!"
+        paragraph: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas totam doloremque adipisci necessitatibus eius ad, quasi nobis illum quam itaque!"
     },
     {
         image: "./img/SAM_0215.jpg",
         title: "Ant",
-        paragraph: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem in eveniet sed nobis. Itaque, autem! Sapiente, non repellendus! Quas totam doloremque adipisci necessitatibus eius ad, quasi nobis illum quam itaque!"
+        paragraph: "Lorem eveniet sed nobis. Itaque, autem! Sapiente, non repellendus! Quas totam doloremque adipisci necessitatibus eius ad, quasi nobis illum quam itaque!"
     },
     {
         image: "./img/SAM_0387.JPG",
@@ -19,32 +19,32 @@ let galleryArr = [
     {
         image: "./img/SAM_0431.jpg",
         title: "Bridge",
-        paragraph: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem in eveniet sed nobis. Itaque, autem! Sapiente, non repellendus! Quas totam doloremque adipisci necessitatibus eius ad, quasi nobis illum quam itaque!"
+        paragraph: "Quidem in eveniet sed nobis. Itaque, autem! Sapiente, non repellendus! Quas totam doloremque adipisci necessitatibus eius ad, quasi nobis illum quam itaque!"
     },
     {
         image: "./img/SAM_1207.jpg",
         title: "Clock",
-        paragraph: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem in eveniet sed nobis. Itaque, autem! Sapiente, non repellendus! Quas totam doloremque adipisci necessitatibus eius ad, quasi nobis illum quam itaque!"
+        paragraph: "Itaque, autem! Sapiente, non repellendus! Quas totam doloremque adipisci necessitatibus eius ad, quasi nobis illum quam itaque!"
     },
     {
         image: "./img/SAM_6494.jpg",
         title: "Blow ball",
-        paragraph: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem in eveniet sed nobis. Itaque, autem! Sapiente, non repellendus! Quas totam doloremque adipisci necessitatibus eius ad, quasi nobis illum quam itaque!"
+        paragraph: "Lorem in eveniet sed nobis. Itaque, autem! Sapiente, non repellendus! Quas totam doloremque adipisci necessitatibus eius ad, quasi nobis illum quam itaque!"
     },
     {
         image: "./img/SAM_6685.jpg",
         title: "Woodpecker",
-        paragraph: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem in eveniet sed nobis. Itaque, autem! Sapiente, non repellendus! Quas totam doloremque adipisci necessitatibus eius ad, quasi nobis illum quam itaque!"
+        paragraph: "Sapiente, non repellendus! Quas totam doloremque adipisci necessitatibus eius ad, quasi nobis illum quam itaque!"
     },
     {
         image: "./img/SAM_8967.jpg",
         title: "Ducks",
-        paragraph: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem in eveniet sed nobis. Itaque, autem! Sapiente, non repellendus! Quas totam doloremque adipisci necessitatibus eius ad, quasi nobis illum quam itaque!"
-    },
+        paragraph: "Quas totam doloremque adipisci necessitatibus eius ad, quasi nobis illum quam itaque!"
+    }
 ];
 
 // LOAD THUMBNAILS
-//for (let i = 0; i < galleryArr.length-1; i++) {
+//for (let i = 0; i < galleryArr.length; i++) {
 for (let i in galleryArr) {
     //$('ul').append(`<li><img src="${galleryArr[i].image}" alt="${galleryArr[i].title}" id="item${i}"></li>`)
     $('ul').append(`<li id="item${i}" style="background-image: url(${galleryArr[i].image})"></li>`)
@@ -88,62 +88,92 @@ function choose() {
         i = item.target.id.slice(4)
         choosePic(i)
 })}
+choose()
 
 ///KEY EVENTS
-$(document).keydown(function(e) {
-    if (e.keyCode == 37) {prev()}
-    else if (e.keyCode == 39) {next()}
-})
-
+function arrowKeys() {
+    $(document).keydown(function(e) {
+        if (e.keyCode == 37) {prev()}
+        else if (e.keyCode == 39) {next()}
+})}
+arrowKeys()
 //$('*').css('opacity', '100')
 
 ///Extra functions
 
 function addView() {
+    $('input').unbind("keydown");
     $('div.upld').css('display', 'block')
     $('#addBtn').css('display', 'none')
+    $('#uplBtn').attr('onclick', 'upload()')
 }
 
 function cancelUpload() {
     $('div.upld').css('display', 'none')
-    $('#addBtn').css('display', 'inline')
+    $('#addBtn').css('display', 'inline-block')
     $('#uploadSrc').val("")
     $('#uploadTitle').val("")
     $('#uploadInfo').val("")
+    $('#delBtn').css('display', 'none')
+    $('#editBtn').css('display', 'inline')
+    //arrowKeys()
 }
 
 function upload() {
     i = galleryArr.length
-    update(galleryArr.length)
+    update(i)
     $('ul').append(`<li id="item${i}" style="background-image: url(${galleryArr[i].image})"></li>`)
     choose()
 }
 
 function update(i) {
+    if ($('#uploadSrc').val() != "") {
+        Arr = $('#uploadSrc').val().split("fakepath")[1].split("")
+        path = "./img/more/"
+        for (let j = 1; j < Arr.length; j++) {
+            path += Arr[j]
+        }
+    } else {
+        path = galleryArr[current].image
+    }
     galleryArr[i] = {
-        image: $('#uploadSrc').val(),
+        image: path,
         title: $('#uploadTitle').val(),
         paragraph: $('#uploadInfo').val()
     }
-    cancelUpload()
     choosePic(i)
+    cancelUpload()
 }
 
 function modify() {
     update(current)
-    $('#uplBtn').attr('onclick', 'upload()')
+    //$('#uplBtn').attr('onclick', 'upload()')
     $('ul').html("")
     for (let i in galleryArr) {
         $('ul').append(`<li id="item${i}" style="background-image: url(${galleryArr[i].image})"></li>`)
     };
     choose()
+    $('#delBtn').css('display', 'none')
 }
 
 function edit() {
-    $('#uploadSrc').val(`${galleryArr[current].image}`)
+    //$('#uploadSrc').val(`${galleryArr[current].image}`)
     $('#uploadTitle').val(`${galleryArr[current].title}`)
     $('#uploadInfo').val(`${galleryArr[current].paragraph}`)
     addView()
+    $('#editBtn').css('display', 'none')
     $('#uplBtn').attr('onclick', 'modify()')
+    $('#delBtn').css('display', 'inline')
 }
-choose()
+
+function del() {
+    c = current
+    galleryArr.splice(c, 1)
+    $('ul').html("")
+    for (let i in galleryArr) {
+        $('ul').append(`<li id="item${i}" style="background-image: url(${galleryArr[i].image})"></li>`)
+    };
+    choose()
+    next()
+    prev()
+}
