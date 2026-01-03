b2bb67d0ad58ac9e152f30f893467b9f7dbe584a
akosaf <faddiakos@gmail.com>
2026-01-03 23:00:32 +0100

    251029_unknown changes

--- MediaCenter/extension/navbar.js
+++ MediaCenter/extension/navbar.js
@@ -9,10 +9,14 @@ if (SKIP_HOSTNAMES.includes(location.hostname)) {
       const nav = document.createElement('div');
       nav.id = 'akos-navbar';
       nav.innerHTML = `
-        <div class="akos-left"><button id="homeBtn" class="btn btn-outline-primary me-2"><svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
-  <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z"/>
-  <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z"/>
-</svg><i class="bi bi-house-fill"></i></button></div>
+        <div class="akos-left">
+          <button id="homeBtn" class="btn btn-outline-primary me-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+            <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z"/>
+            <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z"/>
+            </svg>
+            <i class="bi bi-house-fill"></i>
+          </button></div>
         <div class="akos-center" id="trayApps"></div>
         <div class="akos-right" id="trayRight">
           <div id="sysDate"><span id="currentDate"></span></div>
